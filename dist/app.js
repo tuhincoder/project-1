f2f73d81e8ec7bcd5a8d65819fe2d070d9763220
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-11-18 14:24:22 +0600

    middle ware added

--- dist/app.js
+++ dist/app.js
@@ -9,6 +9,32 @@ const port = 3000;
 // parser
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+//
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/course', courseRouter);
+userRouter.get("/create-user", (req, res) => {
+    try {
+        res.send(somthing);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(400).json({
+            success: false,
+            message: 'failed to get data'
+        });
+    }
+});
+courseRouter.post('/create-course', (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: 'course is start next month',
+        data: course
+    });
+});
 const logger = (req, res, next) => {
     console.log('middle ware request:', req.url, req.method, req.hostname);
     next();
--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,47 @@ const port = 3000;
 app.use(express.json())
 app.use(express.text())
 
+//
+const userRouter = express.Router()
+const courseRouter = express.Router()
+
+app.use('/api/v1/user', userRouter)
+app.use('/api/v1/course', courseRouter)
+
+userRouter.get("/create-user", (req: Request, res: Response) => {
+    try {
+        res.send(somthing)
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            success: false,
+            message: 'failed to get data'
+        })
+    }
+
+
+})
+
+
+courseRouter.post('/create-course', (req: Request, res: Response) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: 'course is start next month',
+        data: course
+    })
+})
+
+
+
+
+
+
+
+
+
+
 
 const logger = (req: Request, res: Response, next: NextFunction) => {
     console.log('middle ware request:', req.url, req.method, req.hostname);
